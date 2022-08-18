fe32c503d30cb0036ba82d56cf66a49d113b4b49
Ganesh Ghadge <89139437+ganesh6465@users.noreply.github.com>
2022-08-18 22:23:14 +0530

    maps in dsa

--- maps.cpp
+++ maps.cpp
@@ -1,6 +1,27 @@
 #include<iostream>
 using namespace std;
-int main(){
+map<int,int> mapInsert(int arr[], int n){
+    map<int, int>mp;
+    //Insert arr[i] as key and i as value
+    for(int i=0;i<n;i++){
+        // mp[key] = value
+        mp[arr[i]] = i;
+    }
+    return mp;
+}
 
+void mapDisplay(map<int,int>mp){
+    // Print every key and value pair in a new line
+    map<int,int>:: iterator it;
+
+    for(it = mp.begin(); it!=mp.end();it++){
+
+    }
+}
+
+void mapErase(map<int,int>mp){
+}
+int main(){
+delta plc
 return 0;
 }
